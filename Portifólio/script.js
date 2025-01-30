9cf675bf1518651251fc0a63779ff1ce37b9c0fe
Vitor Hugo <188543162+uvitordev@users.noreply.github.com>
2025-01-30 17:00:28 -0300

    Add files via upload

---
+++ Portifólio/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.icon-menu');
+    const navLinks = document.getElementById('navLinks');
+
+    menuIcon.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
