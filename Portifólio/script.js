42ca87edd0bb970dc03ae66f18e6b4e88f3a54ef
Vitor Hugo <188543162+uvitordev@users.noreply.github.com>
2025-02-03 11:50:51 -0300

    Update script.js

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.icon-menu');
-    const navLinks = document.getElementById('navLinks');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuIcon = document.querySelector('.icon-menu');
+//     const navLinks = document.getElementById('navLinks');
 
-    menuIcon.addEventListener('click', function() {
-        navLinks.classList.toggle('show');
-    });
-});
+//     menuIcon.addEventListener('click', function() {
+//         navLinks.classList.toggle('show');
+//     });
+// });
